665e51e073493c638a48df3f6fdac033ed13a7ed
Kyu Lee <kwooler@gmail.com>
2026-03-05 13:35:00 -0800

    Student starter code

--- main.cpp
+++ main.cpp
@@ -15,34 +15,6 @@ int main()
     cin >> weight >> distance;
 
     // TODO
-    // Validate weight: 0 < weight <= 20
-    if (weight <= 0 || weight > 20) {
-        cout << "Invalid weight\n";
-        return 0;
-    }
-    // Validate distance: 10 <= distance <= 3000
-    if (distance < 10 || distance > 3000) {
-        cout << "Invalid distance\n";
-        return 0;
-    }
-
-    // Determine rate by weight
-    if (weight <= 2) {
-        rate = rate1;
-    } else if (weight <= 6) {
-        rate = rate2;
-    } else if (weight <= 10) {
-        rate = rate3;
-    } else {
-        rate = rate4;
-    }
-
-    // Calculate charge
-    if (distance > 500) {
-        total_charge = (distance / 500.0) * rate;
-    } else {
-        total_charge = rate;
-    }
 
     cout << setw(10) << left << setprecision(2) << fixed;
     cout << total_charge << endl;
